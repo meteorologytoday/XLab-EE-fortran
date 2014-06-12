a46b02e91c7b4ca6dd67f8696e5d7e571a36b39c
meteorologytoday <meteorologytoday@gmail.com>
2014-06-12 21:25:02 +0800

    Add subroutine print_constants

--- xtt-lib/constants.f90
+++ xtt-lib/constants.f90
@@ -6,4 +6,27 @@ real(4), parameter :: g0=9.8, theta0=298.0, Rd=287.0,  Cv=5.0/2.0*Rd, Cp=Cv + Rd
 
 real(4), parameter :: PI = acos(-1.0), PII = 2*PI
 
+
+contains
+
+subroutine print_constants()
+implicit none
+
+print *, "Constants in xtt-lib/constants.f90 : "
+print *, "Rd      (J/K/kg) : ", Rd
+print *, "Cv      (J/K/kg) : ", Cv
+print *, "Cp      (J/K/kg) : ", Cp
+print *, "kappa            : ", kappa
+print *, "g0      (m/s^2)  : ", g0
+print *, "p0      (pa)     : ", p0
+print *, "theta0  (K)      : ", theta0
+print *, "h0      (m)      : ", h0
+
+
+end subroutine
+
+
+
+
+
 end module
